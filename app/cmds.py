03fd1205135ae21e9b6f96774eeceaa31bff1f49
Adeeb Shihadeh <quillford@users.noreply.github.com>
2017-07-25 16:24:43 -0700

    ubuntu

--- app/cmds.py
+++ app/cmds.py
@@ -2,6 +2,7 @@ import os
 
 def handleCmd(cmd):
   if "roll-up" in cmd:
-    os.system("spotify play uri https://open.spotify.com/track/7kClqlbpmpZmGMimROkvh6")
+    # os.system("spotify play uri https://open.spotify.com/track/7kClqlbpmpZmGMimROkvh6")
+    os.system("qdbus org.mpris.MediaPlayer2.spotify / org.freedesktop.MediaPlayer2.OpenUri https://open.spotify.com/track/7kClqlbpmpZmGMimROkvh6")
     
     print "roll up!"
