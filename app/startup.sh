a2fae7b5396871b4436bc174c74990033f0dd8de
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2017-07-28 23:07:07 -0700

    working script

--- app/startup.sh
+++ app/startup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-exec ./rotate.sh
+exec /home/adeeb/carvis-proto/app/rotate.sh &
 
-python /home/pi/carvis-proto/app/app.py &
+python /home/adeeb/carvis-proto/app/app.py &
 
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/Default/Preferences
-sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/adeeb/.config/chromium/Default/Preferences &
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/adeeb/.config/chromium/Default/Preferences &
 
 chromium-browser http://localhost:5000 &
+
+exit 0
