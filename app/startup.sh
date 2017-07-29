fe3a93b10a99349877bbe9199204fff9d26eba26
Adeeb Shihadeh <quillford@users.noreply.github.com>
2017-07-28 22:26:53 -0700

    automation

--- app/startup.sh
+++ app/startup.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-python /home/pi/carvis-proto/app/app.py
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
+ 
 
+python /home/pi/carvis-proto/app/app.py &
+
+chromium-browser http://localhost:5000 &
 
 # rotate screen & touch input
+
+exec ./rotate.sh
+
 # launch chromium kiosk mode. direct to localhost:5000
 # no screensaver/sleep
