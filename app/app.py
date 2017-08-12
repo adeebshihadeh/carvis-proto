696d3015684dc796db31ea516a37d3ac48a4b4e4
quillford <quillford@users.noreply.github.com>
2017-08-11 17:23:05 -0700

    update

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ import time
 import requests
 import os
 import cmds
+import audio
 
 from flask import Flask, render_template, request, jsonify
 
@@ -12,7 +13,7 @@ app = Flask(__name__)
 def home():
   return render_template('index.html')
 
-@app.route('/command', methods=['GET', 'POST'])
+@app.route('/command', methods=['POST'])
 def command():
   if request.method == 'POST':
     print "post request"
@@ -20,11 +21,18 @@ def command():
     cmds.handleCmd(request.form.get('cmd'))
   return "ok"
 
-@app.route('/update', methods=['GET', 'POST'])
+@app.route('/update', methods=['POST'])
 def update():
   os.system("git pull")
   return render_template('index.html')
 
 
+@app.route('/audio', methods=['POST'])
+def handle_audio():
+  audio.handle_request()
+  request.form.get('cmd')
+  return "ok"
+
+
 if __name__ == '__main__':
-  app.run(debug=True, host= 'localhost')
+  app.run(debug=True, host= 'localhost', port=8080)
