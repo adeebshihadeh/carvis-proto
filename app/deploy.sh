181bc75d1303a2422999bbcf342df83b3e507689
Adeeb Shihadeh <quillford@users.noreply.github.com>
2017-08-11 18:38:18 -0700

    security enhancement

--- app/deploy.sh
+++ app/deploy.sh
@@ -1,9 +1,9 @@
-sshpass -p "raspberry" ssh -tt -o StrictHostKeyChecking=no pi@carvis-proto.local "rm -rf ~/carvis-proto"
+sshpass -p "password" ssh -tt -o StrictHostKeyChecking=no pi@carvis-proto.local "rm -rf ~/carvis-proto"
 
 echo "purged directory... transferring new files" &
 
-sshpass -p "raspberry" rsync -av --exclude "*.git" ../../carvis-proto/ pi@carvis-proto.local:~/carvis-proto
+sshpass -p "password" rsync -av --exclude "*.git" ../../carvis-proto/ pi@carvis-proto.local:~/carvis-proto
 
 echo "new files transferred. starting server" &
 
-sshpass -p "raspberry" ssh -tt -o StrictHostKeyChecking=no pi@carvis-proto.local "fuser -k 8080/tcp && python ~/carvis-proto/app/app.py"
+sshpass -p "password" ssh -tt -o StrictHostKeyChecking=no pi@carvis-proto.local "fuser -k 8080/tcp && python ~/carvis-proto/app/app.py"
