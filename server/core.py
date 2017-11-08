309b2674bf070336ad7227c18f9116f8435e89c4
Adeeb Shihadeh <quillford@users.noreply.github.com>
2017-11-07 21:02:44 -0800

    update

---
+++ server/core.py
@@ -0,0 +1,38 @@
+# 
+
+
+from modules import audio
+from modules import bluetooth
+
+
+
+
+class Core:
+  modules = []
+  update_function = None
+
+  def get_state(self):
+    for module in self.modules:
+      print module.get_state()
+
+  def set_update_function(self, func):
+    self.update_function = func
+    func("")
+
+  def register_module(self, module):
+    self.modules.append(module)
+    module.__init__()
+
+  def broadcast_input(self, input):
+    for module in self.modules:
+      module.handle_input(input)
+
+  def handle_msg(self, msg):
+    self.broadcast_input(msg)
+
+  def __init__(self):
+    return None
+
+core = Core()
+core.register_module(audio.Audio())
+core.register_module(bluetooth.Bluetooth())
---
+++ server/modules/audio.py
@@ -0,0 +1,18 @@
+
+class Audio:
+
+  playing = False
+  source = ""
+
+  def select_source():
+    return ""
+
+  def get_state(self):
+    return "audio ok"
+
+
+  def handle_input(self, input):
+    return ""
+
+  def __init__(self):
+    return None
