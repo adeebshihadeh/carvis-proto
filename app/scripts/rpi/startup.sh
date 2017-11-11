72db53cd0337d34faa4473951be9f422ce8bf3c1
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2017-11-10 19:28:17 -0800

    cleanup

--- app/scripts/rpi/startup.sh
+++ app/scripts/rpi/startup.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-xinput --set-prop 'ILITEK ILITEK Multi-Touch' 'Coordinate Transformation Matrix'  0 1 0 -1 0 1 0 0 1
+exec /home/adeeb/carvis-proto/app/rotate.sh &
 
-python /home/pi/carvis-proto/app/app.py &
+python /home/adeeb/carvis-proto/app/app.py &
 
-xset s noblank
-xset s off
-xset -dpms
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/adeeb/.config/chromium/Default/Preferences &
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/adeeb/.config/chromium/Default/Preferences &
 
-
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
-sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
-
-chromium-browser --noerrdialogs http://localhost:8080 &
-#chromium-browser --noerrdialogs --kiosk http://localhost:8080 &
+chromium-browser http://localhost:5000 &
 
 exit 0
