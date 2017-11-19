bf5121916e699394874c626de87db27242460b5d
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2017-11-19 12:53:27 -0800

    update scripts

--- app/scripts/carvis-start.sh
+++ app/scripts/carvis-start.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-python ~/Desktop/carvis-proto/app/app.py &
+python ~/carvis-proto/app/app.py &
 
 spotify & 
 
-chromium-browser ~/Desktop/carvis-proto/app/loading.html
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/Default/Preferences &
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences &
+
+chromium-browser ~/carvis-proto/app/loading.html
 
 exit 0
